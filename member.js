f09552bd8e230bf33e1caff81600110b9e1ed2ff
rprudhommes <144729481+rprudhommes@users.noreply.github.com>
2023-11-29 03:14:38 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+  return {
+    name: "skills",
+    title: "Skills",
+    description: "Skills",
+    visibility: "private",
+    type: "string",
+    default: "",
+  };
+}
